f2e0fa0ef4ca40a1c130756fe7457e0a2dd92bf7
seanwestfall <sean.westfall@gmail.com>
2019-05-03 16:03:57 -0700

    add placeholder

---
+++ placeholder/php/form-process.php
@@ -0,0 +1,56 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// MESSAGE
+if (empty($_POST["detail"])) {
+    $errorMSG .= "Details is required ";
+} else {
+    $detail = $_POST["detail"];
+}
+
+
+$EmailTo = "aakram23@gmail.com";
+$Subject = "New Message Received";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Details: ";
+$Body .= $detail;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
